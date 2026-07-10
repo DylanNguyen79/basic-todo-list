346cedf72801bc26abcac23bafb279eac90d4aac
DylanNguyen79 <ahuyadug@gmail.com>
2026-07-10 17:45:43 +0700

    remove Update button

--- src/components/ActionButton.jsx
+++ src/components/ActionButton.jsx
@@ -1,8 +1,6 @@
-function ActionButton({ handleUpdate, clearAll, handleDelete }) {
+function ActionButton({ clearAll, handleDelete }) {
   return (
     <>
-      <button onClick={handleUpdate}>Update</button>
-
       <button onClick={handleDelete}>Delete</button>
 
       <button onClick={clearAll}>Clear All</button>
